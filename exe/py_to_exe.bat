7918da8266293078cc7c31483872451571d8bf49
Alex <alexbridgez@gmail.com>
2021-05-04 15:48:22 +1000

    Fixed cd typo

--- exe/py_to_exe.bat
+++ exe/py_to_exe.bat
@@ -1,4 +1,4 @@
-cd "C:\Users\alexb\Documents\Programming\Python\54321\scripts\'\"
+cd "C:\Users\alexb\Documents\Programming\Python\54321\scripts\"
 pyinstaller --onefile --icon="C:\Users\alexb\Documents\Programming\Python\54321\icon\icon.ico" 54321.py
 move "C:\Users\alexb\Documents\Programming\Python\54321\scripts\dist\54321.exe" "C:\Users\alexb\Documents\Programming\Python\54321\exe\"
 cd "C:\Users\alexb\Documents\Programming\Python\54321\scripts"
